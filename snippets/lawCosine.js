482038a4f66410eb52f8ef32f31ee6ad3662b759
David F. Reynolds <67420012+davidFreynolds@users.noreply.github.com>
2021-04-05 13:03:50 -0500

    Update lawCosine.js

--- snippets/lawCosine.js
+++ snippets/lawCosine.js
@@ -1,4 +1,6 @@
-function AngleFromLawOfCosines(side_a, side_b, side_c) {
+function angleFromThreeSides(side_a, side_b, side_c) {
+	// side_a, side_b, and side_c are the lengths of the sides
+	// of the triangle.  side_c is the side across from the angle
 	try {
 		theAngle = Math.acos(
         		(	Math.pow(side_b, 2) +
